0e8a20db2ad0f810bf052a670f58c3dfc6b79a4c
jee-mj <miraj@bhattarai.com.au>
2023-12-17 10:07:41 +1100

    fix for mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,18 @@ export default {
     extend: {
       transitionDuration: {
         2000: "2000ms",
+        2500: "2500ms",
+        3000: "3000ms",
+        3500: "3500ms",
+        4000: "4000ms",
+        4500: "4500ms",
+        5000: "5000ms",
+        5500: "5500ms",
+        6000: "6000ms",
+        6500: "6500ms",
+        7000: "7000ms",
+        7500: "7500ms",
+        8000: "8000ms",
       },
       fontFamily: {
         smooch: ["Smooch Sans", "sans-serif"],
