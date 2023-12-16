9cec4d635abb822610118087c651ca1609606da1
mj <28581723+jee-mj@users.noreply.github.com>
2023-12-17 10:41:35 +1100

    D1 scope issue or something (#1)
    * downsampling
    
    * downsampling without losing quality
    
    * makes the website a little heavier, but it feels like the right thing to do if it works as intended
    
    * [fix?] bugs out in production not in development.
    
    * these things break my dev environment :(
    
    * Trying to fix issue with tsx file paths and font
    
    * so infuriating, and embarassing
    
    * smoother animation
    
    * was the problem because of the `.`? The prior attempt was performed with such a distraught mindset that the test was not performed!
    
    * this should fix the issue with the font
    
    * home page is looking decent for this stage
    
    * fix for mobile
    
    * let's merge back in to main
    
    ---------
    
    Co-authored-by: jee-mj

---
+++ src/components/background-upsampler.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+
+const BackgroundUpsampler = () => {
+  const [currentImage, setCurrentImage] = useState(2);
+  const [nextImage, setNextImage] = useState(3);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const totalImages = 50;
+  const transitionDuration = 2.331;
+
+  const preloadImage = (imageIndex) => {
+    const img = new Image();
+    img.src = `./assets/background/bg-${imageIndex}.jpeg`;
+    img.onload = () => setIsLoaded(true);
+  };
+
+  useEffect(() => {
+    preloadImage(nextImage);
+
+    const interval = setInterval(() => {
+      if (isLoaded) {
+        const newCurrentImage = nextImage;
+        const newNextImage =
+          nextImage < totalImages ? nextImage + 1 : nextImage;
+
+        setCurrentImage(newCurrentImage);
+        setNextImage(newNextImage);
+        setIsLoaded(false);
+
+        preloadImage(newNextImage);
+      }
+    }, 2331);
+
+    return () => clearInterval(interval);
+  }, [isLoaded, nextImage, currentImage]);
+
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full z-0">
+      <motion.div
+        key={currentImage}
+        className="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-cover bg-center"
+        style={{
+          backgroundImage: `url('./assets/background/bg-${currentImage}.jpeg')`,
+        }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: transitionDuration, delay: transitionDuration }}
+      />
+      {nextImage <= totalImages && (
+        <motion.div
+          key={nextImage}
+          className="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-cover bg-center"
+          style={{
+            backgroundImage: `url('./assets/background/bg-${nextImage}.jpeg')`,
+          }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: transitionDuration }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default BackgroundUpsampler;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,13 +5,25 @@ export default {
     extend: {
       transitionDuration: {
         2000: "2000ms",
+        2500: "2500ms",
+        3000: "3000ms",
+        3500: "3500ms",
+        4000: "4000ms",
+        4500: "4500ms",
+        5000: "5000ms",
+        5500: "5500ms",
+        6000: "6000ms",
+        6500: "6500ms",
+        7000: "7000ms",
+        7500: "7500ms",
+        8000: "8000ms",
       },
       fontFamily: {
         smooch: ["Smooch Sans", "sans-serif"],
       },
       colors: {
         primary: "#ffcd0099",
-        secondary: "#012169e6",
+        secondary: "#01216933",
         danger: "#f6511d",
         success: "#2ecf63",
         info: "#39c2e1",
