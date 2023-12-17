b7e75a85b8a28a1b11e5e8c37c5e929b8100641f
jee-mj <miraj@bhattarai.com.au>
2023-12-18 01:42:35 +1100

    animation

--- notes/day2.md
+++ notes/day2.md
@@ -11,3 +11,7 @@ Took most of Day 2 just compressing everything down. I think it's a successful v
 Removed the backgrounds because the most difficult thing about managing that design is not being an 31337 coder that is familiar with JPEG interlacing and lazy loading.
 
 It's fine though, astro has further optimisation strategies for everything later.
+
+It feels a little redundant, but seeing the static background changes the plan. I'll have a loop of 5 jpeg compressions.
+
+Gotta animate movement. 
--- src/components/background-upsampler.tsx
+++ src/components/background-upsampler.tsx
@@ -1,17 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
 const BackgroundUpsampler = () => {
-  const [currentImage, setCurrentImage] = useState(2);
-  const [nextImage, setNextImage] = useState(3);
+  const [currentImage, setCurrentImage] = useState(0);
+  const [nextImage, setNextImage] = useState(1);
   const [isLoaded, setIsLoaded] = useState(false);
-  const totalImages = 50;
-  const transitionDuration = 2.331;
+  const [forward, setForward] = useState(true); // State to track direction
+  const totalImages = 5;
+  const transitionDuration = .5;
+  const preloadedImagesRef = useRef(new Set());
 
   const preloadImage = (imageIndex) => {
+    if (preloadedImagesRef.current.has(imageIndex)) {
+      // Image already preloaded, skip loading
+      return;
+    }
+
     const img = new Image();
     img.src = `./assets/background/bg-${imageIndex}.jpeg`;
-    img.onload = () => setIsLoaded(true);
+    img.onload = () => {
+      setIsLoaded(true);
+      preloadedImagesRef.current.add(imageIndex);
+    };
   };
 
   useEffect(() => {
@@ -19,20 +29,33 @@ const BackgroundUpsampler = () => {
 
     const interval = setInterval(() => {
       if (isLoaded) {
-        const newCurrentImage = nextImage;
-        const newNextImage =
-          nextImage < totalImages ? nextImage + 1 : nextImage;
+        let newNextImage = nextImage;
 
-        setCurrentImage(newCurrentImage);
+        // Update the direction if at the boundaries
+        if (nextImage >= totalImages && forward) {
+          setForward(false);
+        } else if (nextImage <= 0 && !forward) {
+          setForward(true);
+        }
+
+        // Update the nextImage based on the direction
+        if (forward) {
+          newNextImage = (nextImage + 1 <= totalImages) ? nextImage + 1 : nextImage - 1;
+        } else {
+          newNextImage = (nextImage - 1 >= 0) ? nextImage - 1 : nextImage + 1;
+        }
+
+        setCurrentImage(nextImage);
         setNextImage(newNextImage);
         setIsLoaded(false);
 
         preloadImage(newNextImage);
       }
-    }, 2331);
+    }, 500);
 
     return () => clearInterval(interval);
-  }, [isLoaded, nextImage, currentImage]);
+  }, [isLoaded, nextImage, forward]);
+
 
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full z-0">
@@ -46,7 +69,7 @@ const BackgroundUpsampler = () => {
         animate={{ opacity: 1 }}
         transition={{ duration: transitionDuration, delay: transitionDuration }}
       />
-      {nextImage <= totalImages && (
+      {nextImage <= totalImages && nextImage >= 1 && (
         <motion.div
           key={nextImage}
           className="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-cover bg-center"
